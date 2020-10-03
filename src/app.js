262082cd98451c5e3e5afc9b811479a2523a84b8
leandromac <contato@leandromacedo.com>
2020-10-03 09:24:41 -0300

    Start project calculator with reactjs

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React from 'react'
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>App</h1>
+      </div>
+    )
+  }
+})
+
+export default App
